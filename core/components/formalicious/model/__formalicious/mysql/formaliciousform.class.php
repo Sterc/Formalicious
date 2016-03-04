75d8a0ec93c8b01bca56c8edf03248d62cb72c23
Roel Zeilstra <roel@sterc.nl>
2016-03-04 15:18:16 +0100

    Remove backup files.

--- core/components/formalicious/model/__formalicious/mysql/formaliciousform.class.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * @package formalicious
- */
-require_once (strtr(realpath(dirname(dirname(__FILE__))), '\\', '/') . '/formaliciousform.class.php');
-class FormaliciousForm_mysql extends FormaliciousForm {}
-?>
