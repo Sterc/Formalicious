101dee30d25728b06e31f6f12492e8fc123bbbb6
Joeke Kloosterman <joeke@sterc.nl>
2017-01-19 16:59:16 +0100

    Fix return in uninstall resolver + add new transport version

--- _build/resolvers/uninstall.resolver.php
+++ _build/resolvers/uninstall.resolver.php
@@ -6,15 +6,13 @@
  * @subpackage build
  */
 
-$success = true;
 switch ($options[xPDOTransport::PACKAGE_ACTION]) {
     case xPDOTransport::ACTION_UNINSTALL:
         $menuItem = $object->xpdo->getObject('modMenu', 'formalicious');
         if ($menuItem) {
             $menuItem->remove();
         }
-        $success = true;
         break;
 }
 
-return $success;
+return true;
