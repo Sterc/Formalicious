a793787df22c2d3fdcb7f78501b55795fbe367bf
Joeke Kloosterman <joeke@sterc.nl>
2017-05-02 09:05:47 +0200

    Cleanup file

--- core/components/formalicious/processors/mgr/step/reorder.class.php
+++ core/components/formalicious/processors/mgr/step/reorder.class.php
@@ -1,57 +1,27 @@
 <?php
 /**
- * Reorder categories
+ * Reorder steps
  *
  * @package formalicious
  * @subpackage processors
  */
-class FormaliciousReorderStepUpdateProcessor extends modObjectProcessor {
+class FormaliciousReorderStepUpdateProcessor extends modObjectProcessor
+{
     public $classKey = 'FormaliciousStep';
     public $languageTopics = array('formalicious:default');
 
-    public function process(){
+    public function process()
+    {
         $newOrder = $this->getProperty('newOrder');
-        $form_id = $this->getProperty('form_id');
         $i = 1;
         foreach (explode(',', $newOrder) as $step_id) {
             $item = $this->modx->getObject($this->classKey, array('id' => $step_id));
-            if($item){
+            if ($item) {
                 $item->set('rank', $i);
                 $item->save();
                 $i++;
             }
         }
-        // $items = $this->modx->newQuery($this->classKey);
-        // $items->where(array(
-        //         "id:!=" => $idItem,
-        //         "rank:>=" => min($oldIndex, $newIndex),
-        //         "rank:<=" => max($oldIndex, $newIndex),
-        //         'form_id' => $form_id
-        //     ));
-
-        // $items->sortby('rank', 'ASC');
-
-        // $itemsCollection = $this->modx->getCollection($this->classKey, $items);
-
-        // if(min($oldIndex, $newIndex) == $newIndex){
-        //     foreach ($itemsCollection as $item) {
-        //         $itemObject = $this->modx->getObject($this->classKey, $item->get('id'));
-        //         $itemObject->set('rank', $itemObject->get('rank') + 1);
-        //         $itemObject->save();
-        //     }
-        // }else{
-        //     foreach ($itemsCollection as $item) {
-        //         $itemObject = $this->modx->getObject($this->classKey, $item->get('id'));
-        //         $itemObject->set('rank', $itemObject->get('rank') - 1);
-        //         $itemObject->save();
-        //     }
-        // }
-
-        // $itemObject = $this->modx->getObject($this->classKey, $idItem);
-        // $itemObject->set('rank', $newIndex);
-        // $itemObject->save();
-
-
         return $this->success('', array());
     }
 
