c1dd5a3d0990ce4b6f3d8523ee7433789f628591
Gauke Pieter Sietzema <gp@sterc.nl>
2018-12-04 16:26:16 +0100

    Fixed transport.snippets.php

--- _build/data/transport.snippets.php
+++ _build/data/transport.snippets.php
@@ -18,25 +18,33 @@ $snippets[0]->fromArray(array(
 $snippets[1]= $modx->newObject('modSnippet');
 $snippets[1]->fromArray(array(
     'id' => 1,
-    'name' => 'FormaliciousGetValues',
+    'name' => 'FormaliciousHookGetForm',
     'description' => '',
-    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.FormaliciousGetValues.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.formalicioushookgetform.php'),
 ),'',true,true);
 
 $snippets[2]= $modx->newObject('modSnippet');
 $snippets[2]->fromArray(array(
     'id' => 2,
-    'name' => 'FormaliciousSaveValues',
+    'name' => 'FormaliciousHookSetForm',
     'description' => '',
-    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.FormaliciousSaveValues.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.formalicioushooksetform.php'),
 ),'',true,true);
 
 $snippets[3]= $modx->newObject('modSnippet');
 $snippets[3]->fromArray(array(
     'id' => 3,
-    'name' => 'FormaliciousRemoveValues',
+    'name' => 'FormaliciousHookRemoveForm',
     'description' => '',
-    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.FormaliciousRemoveValues.php'),
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.formalicioushookremoveform.php'),
+),'',true,true);
+
+$snippets[4]= $modx->newObject('modSnippet');
+$snippets[4]->fromArray(array(
+    'id' => 4,
+    'name' => 'FormaliciousHookRenderForm',
+    'description' => '',
+    'snippet' => getSnippetContent($sources['source_core'].'/elements/snippets/snippet.formalicioushookrenderform.php'),
 ),'',true,true);
 
 return $snippets;
