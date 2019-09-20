99d6aaa4c2e48da1907468cfa07335f935132821
Kristian Pars <kristian@sterc.nl>
2019-09-20 14:40:40 +0200

    Lexicon correction Dutch

--- core/components/formalicious/lexicon/nl/properties.inc.php
+++ core/components/formalicious/lexicon/nl/properties.inc.php
@@ -8,11 +8,11 @@
 
 $_lang['formalicious']                                          = 'Formalicious';
 
-$_lang['formalicious.snippet_tplform_desc']                     = 'De template van het formulier, dit kan een chunk naam, @FILE of @INLINE zijn.';
-$_lang['formalicious.snippet_tplstep_desc']                     = 'De template van een stap, dit kan een chunk naam, @FILE of @INLINE zijn.';
-$_lang['formalicious.snippet_tplnavigationitem_desc']           = 'De template van een stap in de stappen navigatie, dit kan een chunk naam, @FILE of @INLINE zijn.';
-$_lang['formalicious.snippet_tplnavigationwrapper_desc']        = 'De template van de stappen navigatie, dit kan een chunk naam, @FILE of @INLINE zijn.';
-$_lang['formalicious.snippet_tplemail_desc']                    = 'De template van de email, dit kan alleen een chunk naam zijn.';
-$_lang['formalicious.snippet_tplemailfieldsitem_desc']          = 'De template van de veld in de email, dit kan een chunk naam, @FILE of @INLINE zijn.';
-$_lang['formalicious.snippet_tplemailfieldswrapper_desc']       = 'De template van de wrapper van velden in de email, dit kan een chunk naam, @FILE of @INLINE zijn.';
-$_lang['formalicious.snippet_tplfiaremail_desc']                = 'De template van de auto-reply email, dit kan alleen een chunk naam zijn.';
+$_lang['formalicious.snippet_tplform_desc']                     = 'Het template van het formulier, dit kan een chunk naam, @FILE of @INLINE zijn.';
+$_lang['formalicious.snippet_tplstep_desc']                     = 'Het template van een stap, dit kan een chunk naam, @FILE of @INLINE zijn.';
+$_lang['formalicious.snippet_tplnavigationitem_desc']           = 'Het template van een stap in de stappen navigatie, dit kan een chunk naam, @FILE of @INLINE zijn.';
+$_lang['formalicious.snippet_tplnavigationwrapper_desc']        = 'Het template van de stappen navigatie, dit kan een chunk naam, @FILE of @INLINE zijn.';
+$_lang['formalicious.snippet_tplemail_desc']                    = 'Het template van de e-mail, dit kan alleen een chunk naam zijn.';
+$_lang['formalicious.snippet_tplemailfieldsitem_desc']          = 'Het template van de veld in de e-mail, dit kan een chunk naam, @FILE of @INLINE zijn.';
+$_lang['formalicious.snippet_tplemailfieldswrapper_desc']       = 'Het template van de wrapper van velden in de e-mail, dit kan een chunk naam, @FILE of @INLINE zijn.';
+$_lang['formalicious.snippet_tplfiaremail_desc']                = 'Het template van de auto-reply e-mail, dit kan alleen een chunk naam zijn.';
